5177a5875cfc65f5085f951b32a92a9b0362b02e
Aliciaasmbc <alicia.apreala@gmail.com>
2024-03-30 15:35:13 +0000

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer className="app-footer">
+        <small>
+          This project was coded with 🤍 by {""}
+          <a
+            href="https://www.linkedin.com/in/alicia-apreala-8a26a552/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Alicia Apreala {""}
+          </a>
+          and is {""}
+          <a
+            href="https://github.com/Aliciaasmbc/Dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-sourced on Github
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
